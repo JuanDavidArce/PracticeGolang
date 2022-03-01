c8501c545dd7eedecd556022bcab20e11e34d4d7
Juan David Arce Martinez <juandavid.arce@utp.edu.co>
2022-03-01 15:27:22 -0500

    Slieces

--- tasks.go
+++ tasks.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type taskList struct {
+	tasks []task
+}
+
+func (t *taskList) addToList(tl task) {
+	t.tasks = append(t.tasks, tl)
+}
+
+func (t *taskList) removeFromList(index int) {
+	t.tasks = append(t.tasks[:index], t.tasks[index+1:]...)
+}
+
 type task struct {
 	name        string
 	description string
@@ -21,13 +33,27 @@ func (t *task) updateName(name string) {
 }
 func main() {
 	t := task{
+		name:        "Completar curso de golangooooo",
+		description: "Completar el curso esta semana",
+		complete:    false,
+	}
+	t1 := task{
+		name:        "Completar curso de python",
+		description: "Completar el curso esta semana",
+		complete:    false,
+	}
+	t2 := task{
 		name:        "Completar curso de go",
 		description: "Completar el curso esta semana",
 		complete:    false,
 	}
-	fmt.Printf("%+v\n", t)
-	t.markComplete()
-	t.updateName("FInalizar curso de go")
-	t.updateDescription("Terminar cuato antes")
-	fmt.Printf("%+v\n", t)
+
+	list := taskList{
+		tasks: []task{t1, t2},
+	}
+	fmt.Println(list.tasks[0])
+	list.addToList(t)
+	fmt.Println(len(list.tasks))
+	list.removeFromList(1)
+	fmt.Println(len(list.tasks))
 }
